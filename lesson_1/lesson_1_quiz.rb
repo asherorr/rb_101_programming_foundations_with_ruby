f428fa2b06e36181b0f7e4b8aaa6a91574326567
Asher Orr <asherorr@mailfence.com>
2024-02-06 17:25:10 +0000

    first commit

---
+++ lesson_1/lesson_1_quiz.rb
@@ -0,0 +1,10 @@
+#Write a method that takes one argument, a sentence string, and returns an array of integers. 
+#The value of each integer in the array should be equal to the length of the word in the corresponding position in the sentence. 
+#For example, if the sentence was 'To be or not to be' the array of integers would be [2, 2, 2, 3, 2, 2].
+
+def length_per_word(str_sentence)
+    split_obj = str_sentence.split(" ")
+    final_array = split_obj.map {|obj| obj.length}
+end
+
+p length_per_word("I love programming!")
