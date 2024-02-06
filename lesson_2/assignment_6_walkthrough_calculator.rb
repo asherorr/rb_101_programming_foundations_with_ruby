79b884bfa9d09c398fb8c83eb8b30e9b27e3160d
asherorr <asher@mailfence.com>
2024-02-06 23:31:07 +0000

    This commit will update the assignment_6_walkthrough_calculator.rb file to call the gets and puts methods with their module (Kernel) as per Launch School's instructions for this assignment.

--- lesson_2/assignment_6_walkthrough_calculator.rb
+++ lesson_2/assignment_6_walkthrough_calculator.rb
@@ -1,12 +1,15 @@
+#Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display output. 
+#Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove it: Kernel.gets().chomp().
+
 def calculator
-    puts "Enter the first number >> "
-    num1_str = gets.chomp
+    Kernel.puts("Enter the first number >> ") 
+    num1_str = Kernel.gets().chomp
     num1 = num1_str.to_i
     puts "Enter the second number >> "
-    num2_str = gets.chomp
+    num2_str = Kernel.gets().chomp
     num2 = num2_str.to_i
     puts "Enter the operation (add, subtract, multiply, or divide) >> "
-    operator = gets.chomp
+    operator = Kernel.gets().chomp
     case
     when operator == "add"
         result = num1 + num2
@@ -17,7 +20,7 @@ def calculator
     when operator == "divide"
         result = (num1.to_f / num2.to_f)
     end
-    puts result
+    Kernel.puts(result) 
 end
 
 calculator
