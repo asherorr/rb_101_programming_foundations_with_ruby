4452a2f5d86015a70afddfc45c8fbe4f584a99c0
asher@mailfence.com <asherorr@mailfence.com>
2024-02-06 23:09:38 +0000

    This commit will add the code I wrote for the calculator exercise (lesson 2, assignment 6).

---
+++ lesson_2/assignment_6_walkthrough_calculator.rb
@@ -0,0 +1,23 @@
+def calculator
+    puts "Enter the first number: "
+    num1_str = gets.chomp
+    num1 = num1_str.to_i
+    puts "Enter the second number: "
+    num2_str = gets.chomp
+    num2 = num2_str.to_i
+    puts "Enter the operation: add, subtract, multiply, or divide"
+    operator = gets.chomp
+    case
+    when operator == "add"
+        result = num1 + num2
+    when operator == "subtract"
+        result = num1 - num2
+    when operator == "multiply"
+        result = num1 * num2
+    when operator == "divide"
+        result = (num1.to_f / num2.to_f)
+    end
+    puts result
+end
+
+calculator
