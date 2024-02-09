661d176c14b1667a65bf56bfaf1ad561725e04a4
asherorr <asher@mailfence.com>
2024-02-09 16:49:27 +0000

    This commit will edit files in the lesson_1 and lesson_2 directories to abide by Ruby style guidelines.

--- lesson_2/assignment_6_calculator.rb
+++ lesson_2/assignment_6_calculator.rb
@@ -1,25 +1,25 @@
-#Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display output. 
-#Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove it: Kernel.gets().chomp().
+# Use the Kernel.gets() method to retrieve user input, and use Kernel.puts() method to display output.
+# Remember that Kernel.gets() includes the newline, so you have to call chomp() to remove it: Kernel.gets().chomp().
 
 def calculator
-    Kernel.puts("Enter the first number >> ") 
-    num1 = Kernel.gets().chomp.to_i
-    puts "Enter the second number >> "
-    num2 = Kernel.gets().chomp.to_i
-    puts "Enter the operation (add, subtract, multiply, or divide) >> "
-    operator = Kernel.gets().chomp
-    
-    case
-    when operator == "add"
-        result = num1 + num2
-    when operator == "subtract"
-        result = num1 - num2
-    when operator == "multiply"
-        result = num1 * num2
-    when operator == "divide"
-        result = (num1.to_f / num2.to_f)
-    end
-    Kernel.puts(result) 
+  Kernel.puts('Enter the first number >> ')
+  num1 = Kernel.gets.chomp.to_i
+  puts 'Enter the second number >> '
+  num2 = Kernel.gets.chomp.to_i
+  puts 'Enter the operation (add, subtract, multiply, or divide) >> '
+  operator = Kernel.gets.chomp
+
+  case operator
+  when 'add'
+    result = num1 + num2
+  when 'subtract'
+    result = num1 - num2
+  when 'multiply'
+    result = num1 * num2
+  when 'divide'
+    result = (num1.to_f / num2)
+  end
+  Kernel.puts(result)
 end
 
 calculator
--- lesson_2/assignment_7_psuedocode.rb
+++ lesson_2/assignment_7_psuedocode.rb
@@ -1,27 +1,27 @@
-#def sum
-    #get user input for num1 and convert to integer
-    #get user input for num2 and convert to integer
-    #result = perform operation adding num1 to num2
-    
-#def str_concat
-    #set variable new_string
-    #iterate over each string in the array
-    #concat string to new_string array
-    
-#def return_new_array(array_obj)
-    #initialize new array
-    #iterate over every other item in the array and append to the new array
-    
-#def problem(string, character_selected)
-    #if the character specified doesn’t occur at least 3 times, return nil. 
-    #initialize a counter and set it to 0.
-    #initialize a variable called str_obj_to_a. It will store the split method called on str_obj
-    #iterate over each element in the array
-    #if the element matches the letter provided, update the counter by 1.
-    #when the counter hits the last letter in the string object, return that letter’s index.
-  
-#actual code for the example above:      
-#def problem(str_obj, letter)
+# def sum
+# get user input for num1 and convert to integer
+# get user input for num2 and convert to integer
+# result = perform operation adding num1 to num2
+
+# def str_concat
+# set variable new_string
+# iterate over each string in the array
+# concat string to new_string array
+
+# def return_new_array(array_obj)
+# initialize new array
+# iterate over every other item in the array and append to the new array
+
+# def problem(string, character_selected)
+# if the character specified doesn’t occur at least 3 times, return nil.
+# initialize a counter and set it to 0.
+# initialize a variable called str_obj_to_a. It will store the split method called on str_obj
+# iterate over each element in the array
+# if the element matches the letter provided, update the counter by 1.
+# when the counter hits the last letter in the string object, return that letter’s index.
+
+# actual code for the example above:
+# def problem(str_obj, letter)
 #     return nil if str_obj.count(letter) < 3
 #     counter = 0
 #     str_obj_to_a = str_obj.split("")
@@ -30,34 +30,33 @@
 #         return i if counter == str_obj.count(letter)
 #     end
 # end
-  
-#call the problem method and view the method's return value using p.   
-#p problem("asherasherasher", "a")
+
+# call the problem method and view the method's return value using p.
+# p problem("asherasherasher", "a")
 
 # def merge_array(array1, array2)
-#Initialize a method called merge_arrays, that accepts two arguments: array1 and array2
-#Initialize an empty array to store the result (final_array)
-#Iterate over each index in array1.
-#Append the value at array1[index] to final_array
-#Append the value at array2[index] to the final array.
-#Return the final merged array
+# Initialize a method called merge_arrays, that accepts two arguments: array1 and array2
+# Initialize an empty array to store the result (final_array)
+# Iterate over each index in array1.
+# Append the value at array1[index] to final_array
+# Append the value at array2[index] to the final array.
+# Return the final merged array
 
-#actual code for the example above:   
+# actual code for the example above:
 # def merge_array(array1, array2)
 #     result = array1.zip(array2).flatten
 # end
 
-#another solution
+# another solution
 # def merge_array(array1, array2)
 #   final_array = []
 
-#   array1.each_index do |index|
-#     final_array << array1[index]
-#     final_array << array2[index]
-#   end
-
-#   final_array
+# array1.each_index do |index|
+#   final_array << array1[index]
+#   final_array << array2[index]
 # end
-    
+
+# final_array
+# end
 
 # p merge_array([1, 2, 3], [4, 5, 6])
