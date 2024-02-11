7bb639d79094759265f9bbd9fd02a52578cbf910
asherorr <asher@mailfence.com>
2024-02-11 18:09:13 +0000

    This commit will refactor the case statement in the calculator method for better readability.

--- lesson_2/assignment_6_calculator.rb
+++ lesson_2/assignment_6_calculator.rb
@@ -6,22 +6,22 @@ end
 
 def calculator
   prompt("Welcome to the Calculator!")
-  prompt('Enter the first number >> ')
+  prompt('Enter the first number: ')
   num1 = Kernel.gets.chomp.to_i
-  prompt('Enter the second number >> ')
+  prompt('Enter the second number: ')
   num2 = Kernel.gets.chomp.to_i
-  prompt('Enter the operation (add, subtract, multiply, or divide) >> ')
+  prompt('Enter the operation (add, subtract, multiply, or divide):')
   operator = Kernel.gets.chomp
 
-  case operator
+  result = case operator
   when 'add'
-    result = num1 + num2
+      num1 + num2
   when 'subtract'
-    result = num1 - num2
+      num1 - num2
   when 'multiply'
-    result = num1 * num2
+      num1 * num2
   when 'divide'
-    result = (num1.to_f / num2)
+      num1.to_f / num2
   end
   prompt("The result is #{result}.")
 end
