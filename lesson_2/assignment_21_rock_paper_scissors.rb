9576f4a96ecd63ae37d5f8ed21eb4b97615fa80b
asherorr <asher@mailfence.com>
2024-02-18 19:49:27 +0000

    This commit will add the compare_user_and_computer_selection method.

--- lesson_2/assignment_21_rock_paper_scissors.rb
+++ lesson_2/assignment_21_rock_paper_scissors.rb
@@ -2,6 +2,7 @@ def welcome_message
   puts "Welcome to Rock, Paper, or Scissors!"
 end
 
+
 def ask_user_to_select_option
   loop do
     puts "Select an option:\n
@@ -13,7 +14,7 @@ def ask_user_to_select_option
     options = {"1": "Rock", "2": "Paper", "3": "Scissors"}
     if options.has_key?(selection)
       puts "You selected: #{options[selection]}"
-      return selection
+      return options[selection]
     else
       puts "That is not a valid option. Please enter only 1, 2, or 3."
     end
@@ -22,6 +23,27 @@ end
   
 
 def computer_selects_option
-  options = [:"1", :"2", ":3"]
-  selection = options.sample
+  options = ["Rock", "Paper", "Scissors"]
+  options.sample
+end
+
+
+def show_computer_selection(selection)
+  puts "The computer chose #{selection}"
+end
+
+
+def compare_user_and_computer_selection(user_selection, computer_selection)
+  conditions = {
+    "Rock": "Scissors",
+    "Paper": "Rock",
+    "Scissors": "Paper"
+  }
+  if conditions.key(computer_selection).to_s == user_selection
+    puts "You won!"
+  elsif user_selection == computer_selection
+    puts "Draw!"
+  else
+    puts "You lost!"
+  end
 end
