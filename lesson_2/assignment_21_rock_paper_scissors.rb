5af83cef27b8f3d88209ff074d300b66265409ef
asherorr <asher@mailfence.com>
2024-02-20 00:22:50 +0000

    This commit will refactor the return_user_choice and display_who_won methods to include two more options - Lizard and Spock.

--- lesson_2/assignment_21_rock_paper_scissors.rb
+++ lesson_2/assignment_21_rock_paper_scissors.rb
@@ -9,10 +9,12 @@ def return_user_choice
     puts "1 = Rock"
     puts "2 = Paper"
     puts "3 = Scissors"
+    puts "4 = Spock"
+    puts "5 = Lizard"
     puts "\nEnter a number: "
     selection = gets.chomp.to_sym
     
-    options = {"1": "Rock", "2": "Paper", "3": "Scissors"}
+    options = {"1": "Rock", "2": "Paper", "3": "Scissors", "4": "Spock", "5": "Lizard"}
     if options.has_key?(selection)
       puts "You selected: #{options[selection]}"
       return options[selection]
@@ -24,7 +26,7 @@ end
 
 
 def return_computer_choice
-  options = %w(Rock, Paper, Scissors)
+  options = %w(Rock Paper Scissors Lizard Spock)
   options.sample
 end
 
@@ -36,11 +38,15 @@ end
 
 def display_who_won(user_selection, computer_selection)
   conditions = {
-    "Rock": "Scissors",
-    "Paper": "Rock",
-    "Scissors": "Paper"
+    "Rock": ["Scissors", "Lizard"],
+    "Paper": ["Rock", "Spock"],
+    "Scissors": ["Paper", "Lizard"],
+    "Spock": ["Scissors", "Rock"],
+    "Lizard": ["Spock", "Paper"]
   }
-  if conditions.key(computer_selection).to_s == user_selection
+  user_selection_to_sym = user_selection.to_sym
+  array_obj = conditions[user_selection_to_sym]
+  if array_obj.include?(computer_selection)
     puts "You won!"
   elsif user_selection == computer_selection
     puts "Draw!"
@@ -63,8 +69,8 @@ end
 
 def main
   welcome_message
-  users_selection = return_user_selection
-  computers_selection = return_computer_selection
+  users_selection = return_user_choice
+  computers_selection = return_computer_choice
   show_computer_selection(computers_selection)
   display_who_won(users_selection, computers_selection)
   prompt_user_to_play_again
